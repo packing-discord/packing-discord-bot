9d6810b6c0038e94ca7a87fa919299037b3ad30c
Androz2091 <androz2091@gmail.com>
2021-03-28 21:54:18 +0200

    :sparkles: Add pg password

--- db.ts
+++ db.ts
@@ -3,7 +3,8 @@ import { Pool } from 'pg';
 
 const pool = new Pool({
     user: process.env.PG_USER!,
-    database: process.env.PG_DATABASE!
+    database: process.env.PG_DATABASE!,
+    password: process.env.PG_PASSWORD!
 });
 
 const getCurrentDay = () => {
