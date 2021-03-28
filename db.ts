d308d5b0747d4973cebe0035324e48a200d0d22e
Androz2091 <androz2091@gmail.com>
2021-03-28 21:50:06 +0200

    :sparkles: First commit

---
+++ db.ts
@@ -0,0 +1,72 @@
+import { Snowflake } from 'discord.js';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    user: process.env.PG_USER!,
+    database: process.env.PG_DATABASE!
+});
+
+const getCurrentDay = () => {
+    const date = new Date();
+    return new Date(`${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`).toISOString();
+}
+
+export const startVoiceActivity = (userID: Snowflake, channelID: Snowflake) => {
+    return pool.query(`
+        INSERT INTO voice_activity
+        (user_id, start_date, end_date, channel_id) VALUES
+        ($1, $2, $3, $4);
+    `, [userID, new Date().toISOString(), null, channelID]);
+};
+
+export const endVoiceActivity = (userID: Snowflake) => {
+    return pool.query(`
+        UPDATE voice_activity
+        SET end_date = $1
+        WHERE user_id = $2
+        AND end_date is null;
+    `, [userID]);
+}
+
+export const terminateVoiceActivities = async () => {
+    const { rows } = await pool.query(`
+        SELECT last_ping FROM bot_statistics;
+    `);
+    const lastPing = rows[0].last_ping;
+    return pool.query(`
+        UPDATE voice_activity
+        SET end_date = $1
+        WHERE end_date = $2;
+    `, lastPing);
+}
+
+export const savePing = () => {
+    return pool.query(`
+        UPDATE bot_statistics
+        SET last_ping = $1;
+    `, [new Date().toISOString()]);
+}
+
+export const addMessage = async (userID: Snowflake, channelID: Snowflake) => {
+    const { rows } = await pool.query(`
+        SELECT * FROM message_activity
+        WHERE user_id = $1
+        AND date = $2;
+    `, [userID, getCurrentDay()]);
+    if (rows.length === 0) {
+        await pool.query(`
+            INSERT INTO message_activity
+            (user_id, date, message_count, channels_ids) VALUES
+            ($1, $2, $3);
+        `, [userID, getCurrentDay(), 1, [channelID]]);
+    } else {
+        const previousChannelsIDs = rows[0].channels_ids as Snowflake[];
+        await pool.query(`
+            UPDATE message_activity
+            SET message_count = message_count + 1,
+                channels_ids = $1
+            WHERE user_id = $2
+            AND date = $3;
+        `, [previousChannelsIDs.includes(channelID) ? previousChannelsIDs : [...previousChannelsIDs, channelID], userID, getCurrentDay()]);
+    }
+}
---
+++ index.ts
@@ -0,0 +1,62 @@
+import { config } from 'dotenv';
+import {
+
+    savePing,
+
+    // addScoreEvent,
+
+    startVoiceActivity,
+    endVoiceActivity,
+    terminateVoiceActivities,
+
+    addMessage,
+
+    // getWinLeaderboard,
+    // getVoiceLeaderboard,
+    // getMessagesLeaderboard
+} from './db';
+import { Client, VoiceChannel } from 'discord.js';
+
+// configure environment variables
+config();
+
+setInterval(() => savePing(), 5000);
+
+const client = new Client();
+
+client.on('ready', () => {
+    terminateVoiceActivities();
+    client.channels.cache.filter((channel) => channel.type === 'voice').forEach((channel) => {
+        (channel as VoiceChannel).members.forEach((member) => {
+            startVoiceActivity(member.user.id, channel.id);
+        });
+    });
+});
+
+client.on('voiceStateUpdate', async (oldState, newState) => {
+
+    if (!newState.member) return;
+
+    // if someone joined a channel
+    if (!oldState.channelID && newState.channelID) {
+        startVoiceActivity(newState.member.id, newState.channelID);
+        console.log(`[+] New voice activity created for user ${newState.member.user.username}`);
+    }
+
+    // if someone left a channel
+    else if (oldState.channelID && !newState.channelID) {
+        endVoiceActivity(newState.member.id);
+        console.log(`[x] Voice activity ended for user ${newState.member.user.username}`);
+    }
+
+});
+
+client.on('message', (message) => {
+
+    if (message.author.bot) return;
+
+    addMessage(message.author.id, message.channel.id);
+
+});
+
+client.login(process.env.TOKEN!);
