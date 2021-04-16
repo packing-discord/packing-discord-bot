11b3beaeaa698d653ad46c651564796824efa332
Androz2091 <androz2091@gmail.com>
2021-04-16 19:21:03 +0200

    :sparkles: host channel

--- commands/host.ts
+++ commands/host.ts
@@ -1,27 +1,66 @@
-import { CommandContext, SlashCommand, SlashCreator } from 'slash-create'
+import { MessageEmbed, TextChannel } from 'discord.js';
+import { SlashCreator, SlashCommand, CommandContext } from 'slash-create';
 import client from '../';
+import { createVoiceChannel } from '../db'
 
-export default class extends SlashCommand {
-    constructor(creator: SlashCreator) {
+export default class HostCommand extends SlashCommand {
+    constructor (creator: SlashCreator) {
         super(creator, {
             name: 'host',
-            description: 'Create a host channel',
+            description: 'Create an event channel!',
             options: [
                 {
-                    type: 5,
-                    name: 'stage',
-                    description: 'Whether the channel should be a stage channel or not'
+                    name: 'name',
+                    description: 'How should we call this channel?',
+                    type: 3,
+                    required: true
                 }
             ],
-            requiredPermissions: [],
             guildIDs: [process.env.GUILD_ID!]
         });
+
+        // Not required initially, but required for reloading with a fresh file.
         this.filePath = __filename;
     }
 
-    async run(ctx: CommandContext) {
-        if (!ctx.guildID || !ctx.member) return;
-        const stage = ctx.options.stage as boolean;
-        const guild = client.guilds.cache.get(ctx.guildID)!;
+    async run (ctx: CommandContext) {
+        const hasHostRole = ctx.member?.roles.some((roleID) => roleID === process.env.HOST_ROLE!)!;
+        if (!hasHostRole) {
+            ctx.send('You must have the Host role to create an event channel.', {
+                ephemeral: true
+            });
+            return;
+        }
+
+        const server = client.guilds.cache.get(process.env.GUILD_ID!);
+        server?.channels.create(ctx.options.name as string, {
+            type: 'voice',
+            permissionOverwrites: [
+                {
+                    id: ctx.guildID!,
+                    deny: ['SPEAK']
+                },
+                {
+                    id: process.env.HOST_ROLE!,
+                    allow: ['PRIORITY_SPEAKER', 'MUTE_MEMBERS', 'DEAFEN_MEMBERS']
+                }
+            ],
+            parent: process.env.HOST_CHANNELS_CATEGORY
+        }).then(async (channel) => {
+
+            await createVoiceChannel(channel.id, ctx.user.id);
+            const user = await client.users.fetch(ctx.user.id);
+            const embed = new MessageEmbed()
+                .setAuthor(`Creation of event channel by ${user.tag}`, user.displayAvatarURL())
+                .setDescription(`The event (${channel.name}) has started`)
+                .setColor('#00FF00');
+
+            (client.channels.cache.get(process.env.HOST_LOGS_CHANNEL!)! as TextChannel).send(embed);
+
+            channel.createInvite().then((invite) => {
+                ctx.editOriginal(`:white_check_mark: Your channel ${ctx.options.name as string} has been created. Click the invite to join it! ${invite.url}`);
+            });
+        });
+        
     }
 }
