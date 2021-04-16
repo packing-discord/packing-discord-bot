8167783209bd02bba64f21463f4509bd6e865cf6
Androz2091 <androz2091@gmail.com>
2021-04-16 18:46:15 +0200

    update products

--- products.ts
+++ products.ts
@@ -1,20 +1,42 @@
 export default [
     {
         id: 1,
-        name: '$5 PayPal',
-        paypal: 5,
+        paypal: 10,
         points: 20
     },
     {
         id: 2,
-        name: '$10 PayPal',
-        paypal: 10,
-        points: 30
+        paypal: 20,
+        points: 40
     },
     {
         id: 3,
-        name: '$50 PayPal',
+        paypal: 30,
+        points: 60
+    },
+    {
+        id: 4,
+        paypal: 40,
+        points: 80
+    },
+    {
+        id: 5,
         paypal: 50,
-        points: 50
+        points: 100
+    },
+    {
+        id: 6,
+        paypal: 60,
+        points: 120
+    },
+    {
+        id: 7,
+        paypal: 75,
+        points: 150
+    },
+    {
+        id: 8,
+        paypal: 100,
+        points: 200
     }
 ];
